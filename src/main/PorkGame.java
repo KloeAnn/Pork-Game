cee406800b1f4dadba3e93e8a26c3d5742852c46
KloeAn <KLOE.AN@OOCL.COM>
2019-08-17 18:39:49 +0800

    refactor:PorkGame.java

--- src/main/PorkGame.java
+++ src/main/PorkGame.java
@@ -8,15 +8,10 @@ public class PorkGame {
     private List<Player> players;
 
     public PorkGame(String c) {
-        if (c.length() == 5) {
-            Player player1 = new Player(c.substring(0, 2));
-            Player player2 = new Player(c.substring(3));
-            this.players = Arrays.asList(player1, player2);
-        } else {
-            Player player1 = new Player(c.substring(0, 14));
-            Player player2 = new Player(c.substring(15));
-            this.players = Arrays.asList(player1, player2);
-        }
+        int middleIndex=(c.length()-1)/2;
+        Player player1 = new Player(c.substring(0, middleIndex));
+        Player player2 = new Player(c.substring(middleIndex+1));
+        this.players = Arrays.asList(player1, player2);
     }
 
     public List<Player> getPlayers() {
@@ -35,60 +30,69 @@ public class PorkGame {
             System.out.println(level1.getCardsLevel());
             System.out.println(level2.getCardsLevel());
             return compareResult(level1.getCardsLevel(),level2.getCardsLevel());
-        }else if(level1.getCardsLevel()==6){
-            System.out.println(level1.getCardsLevel());
-            System.out.println(level2.getCardsLevel());
-            List<Integer> list1 = level1.getFlushNumber();
-            List<Integer> list2 = level2.getFlushNumber();
-            for(int i=4;i>=0;i--){
-                String result=compareResult(list1.get(i),list2.get(i));
-                if(!result.equals("This game is a draw"))
-                    return result;
+        }else{
+            switch (level1.getCardsLevel()){
+                case 6:
+                    return compareWhenLevelIs6(level1,level2);
+                case 3:
+                    return compareWhenLevelIs3(level1,level2);
+                case 2:
+                    return compareWhenLevelIs2(level1,level2);
+                case 1:
+                    return compareWhenLevelIs1(level1,level2);
+                default:{
+                    System.out.println(level1.getCardsLevel());
+                    System.out.println(level2.getCardsLevel());
+                    return compareResult(level1.getMaxCard(),level2.getMaxCard());
+                }
+
             }
-            return "This game is a draw";
-        }else if(level1.getCardsLevel()==3){
-            System.out.println(level1.getCardsLevel());
-            System.out.println(level2.getCardsLevel());
-            List<Integer> list1 = Arrays.asList(level1.getNoRepeatNumber().get(0),level1.getRepeatNumber().get(0),level1.getRepeatNumber().get(1));
-            List<Integer> list2 = Arrays.asList(level2.getNoRepeatNumber().get(0),level2.getRepeatNumber().get(0),level2.getRepeatNumber().get(1));
-//            System.out.println(list1);
-//            System.out.println(list2);
-            for(int i=2;i>=0;i--){
-                String result=compareResult(list1.get(i),list2.get(i));
-                if(!result.equals("This game is a draw"))
-                    return result;
-            }
-            return "This game is a draw";
-        }else if(level1.getCardsLevel()==2){
-            System.out.println(level1.getCardsLevel());
-            System.out.println(level2.getCardsLevel());
-            List<Integer> list1 =level1.getNoRepeatNumber();
-            list1.add(level1.getRepeatNumber().get(0));
-            List<Integer> list2 =level2.getNoRepeatNumber();
-            list2.add(level2.getRepeatNumber().get(0));
-            for(int i=3;i>=0;i--){
-                String result=compareResult(list1.get(i),list2.get(i));
-                if(!result.equals("This game is a draw"))
-                    return result;
-            }
-            return "This game is a draw";
-        }else if(level1.getCardsLevel()==1){
-            System.out.println(level1.getCardsLevel());
-            System.out.println(level2.getCardsLevel());
-            List<Integer> list1 =level1.getNoRepeatNumber();
-            List<Integer> list2 =level2.getNoRepeatNumber();
-            for(int i=list1.size()-1;i>=0;i--){
-                String result=compareResult(list1.get(i),list2.get(i));
-                if(!result.equals("This game is a draw"))
-                    return result;
-            }
-            return "This game is a draw";
         }
-        else {
-            System.out.println(level1.getCardsLevel());
-            System.out.println(level2.getCardsLevel());
-            return compareResult(level1.getMaxCard(),level2.getMaxCard());
+    }
+
+    public String compareWhenLevelIs6(Level level1,Level level2){
+        System.out.println(level1.getCardsLevel());
+        System.out.println(level2.getCardsLevel());
+        List<Integer> list1 = level1.getFlushNumber();
+        List<Integer> list2 = level2.getFlushNumber();
+        return compareMutiTimes(list1,list2);
+    }
+
+    public String compareWhenLevelIs3(Level level1,Level level2){
+        System.out.println(level1.getCardsLevel());
+        System.out.println(level2.getCardsLevel());
+        List<Integer> list1 =level1.getNoRepeatNumber();
+        list1.addAll(level1.getRepeatNumber());
+        List<Integer> list2 =level2.getNoRepeatNumber();
+        list2.addAll(level2.getRepeatNumber());
+        return compareMutiTimes(list1,list2);
+    }
+
+    public String compareWhenLevelIs2(Level level1,Level level2){
+        System.out.println(level1.getCardsLevel());
+        System.out.println(level2.getCardsLevel());
+        List<Integer> list1 =level1.getNoRepeatNumber();
+        list1.add(level1.getRepeatNumber().get(0));
+        List<Integer> list2 =level2.getNoRepeatNumber();
+        list2.add(level2.getRepeatNumber().get(0));
+        return compareMutiTimes(list1,list2);
+    }
+
+    public String compareWhenLevelIs1(Level level1,Level level2){
+        System.out.println(level1.getCardsLevel());
+        System.out.println(level2.getCardsLevel());
+        List<Integer> list1 =level1.getNoRepeatNumber();
+        List<Integer> list2 =level2.getNoRepeatNumber();
+        return compareMutiTimes(list1,list2);
+    }
+
+    public String compareMutiTimes(List<Integer>list1,List<Integer>list2){
+        for(int i=list1.size()-1;i>=0;i--){
+            String result=compareResult(list1.get(i),list2.get(i));
+            if(!result.equals("This game is a draw"))
+                return result;
         }
+        return "This game is a draw";
     }
 
     public String compareResult(int number1,int number2){
